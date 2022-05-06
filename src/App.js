4872eef4671ed79cea22f6b9a5109a8a60c4859f
saintmay86400 <mwaynestmay@gmail.com>
2022-05-06 02:07:19 +0200

    prova

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ciao, mi chiamo <code>Mario</code> e verrò a prenderti.
         </p>
         <a
           className="App-link"
