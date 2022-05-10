d9a6eaa7cc5db4bb84d444183f21be0d76859af9
saintmay86400 <mwaynestmay@gmail.com>
2022-05-11 01:04:02 +0200

    studio dello stato e del lifecycle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
           Ciao, mi chiamo <code>Mario</code> e sto studiando <code>React</code>.
           Come mai di disturbi?
         </p>
+        <div>
+        <Clock />
+        <Clock />
+        <Clock />
+        </div>
         <a
           className="App-link"
           href="#"
@@ -22,6 +28,38 @@ function App() {
     </div>
   );
 }
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()};
+  }
+  componentDidMount(){
+
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+
+  }
+  componentWillUnmount(){
+
+    clearInterval(this.timerID);
+
+  }
+  tick(){
+    this.setState( { date: new Date() } );
+  }
+  render(){
+    return (
+      <div>
+        <h1>Ciao Mondo</h1>
+        <h2>Sono le {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+/*
 function Avatar(props){
   return (
     <img className='Avatar'
@@ -49,10 +87,11 @@ function Commento(props){
         {props.testo}
       </div>
       <div className='Commento-data'>
-        {/*formatDate(props.data)*/}
+        {/*formatDate(props.data)}
         {props.data}
       </div>
     </div>
   );
 }
+*/
 export default App;
